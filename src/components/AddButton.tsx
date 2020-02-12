42fcbd27e9152064a26801c5e3ce815bad29d16e
Ben Adam <benjamin.mark.adam@gmail.com>
2020-02-12 13:54:47 -0700

    Fix button hover

--- src/components/AddButton.tsx
+++ src/components/AddButton.tsx
@@ -5,8 +5,9 @@ export default function AddButton({ onClick }: { onClick: () => void }) {
   return (
     <Box position="fixed" bottom="40px" right="40px">
       <Button
+        variantColor="purple"
         backgroundColor="#394AB6"
-        color='white'
+        color="white"
         width="40px"
         height="40px"
         borderRadius={100}
