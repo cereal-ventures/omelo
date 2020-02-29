fc9475fbfc7755378f31d4f1199128b9a00c78c4
Ben Adam <benjamin.mark.adam@gmail.com>
2020-02-29 11:22:59 -0700

    Add tooltip for add event

--- src/components/AddButton.tsx
+++ src/components/AddButton.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
-import { Icon, Box, Button } from '@chakra-ui/core';
+import { Icon, Box, Button, Tooltip } from '@chakra-ui/core';
 
 export default function AddButton({ onClick }: { onClick: () => void }) {
   return (
     <Box position='fixed' bottom='40px' right='40px'>
-      <Button
-        variantColor='purple'
-        width='40px'
-        height='40px'
-        borderRadius='full'
-        onClick={onClick}
+      <Tooltip
+        hasArrow
+        aria-label='Add Event'
+        label='Add Event'
+        placement='top'
       >
-        <Icon name='add' />
-      </Button>
+        <Button
+          variantColor='purple'
+          width='40px'
+          height='40px'
+          borderRadius='full'
+          onClick={onClick}
+        >
+          <Icon name='add' />
+        </Button>
+      </Tooltip>
     </Box>
   );
 }
