b71ed598c949c63189537aa6c5aa129e38047155
Ben Adam <benjamin.mark.adam@gmail.com>
2020-03-07 06:34:31 -0700

    Display avatars for team members

--- src/components/ProjectTitle.tsx
+++ src/components/ProjectTitle.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
-import { Heading, Flex, Input, Button } from '@chakra-ui/core';
+import { Heading, Flex, Input, Button, Avatar, Box } from '@chakra-ui/core';
 import ShareModal from './ShareModal';
 
 import { updateProject } from '../services/data';
@@ -8,10 +8,12 @@ import { updateProject } from '../services/data';
 export default function ProjectTitle({
   projectName,
   projectId,
-  setIsPanelOpen
+  setIsPanelOpen,
+  users = []
 }: {
   projectName: string;
   projectId: string;
+  users?: Array<string>;
   setIsPanelOpen: () => void;
 }) {
   const { register, handleSubmit } = useForm();
@@ -35,6 +37,33 @@ export default function ProjectTitle({
       top='0px'
       p={8}
     >
+      <Box position='absolute' top={24}>
+        <Heading as='h4' fontSize='12px' textTransform='uppercase' mb={4}>
+          Teammates:
+        </Heading>
+        {users.map((user: any) => (
+          <Flex key={user?.uid} mb={4} align='center'>
+            <Avatar
+              src={user.photoUrl}
+              display='block'
+              size='xs'
+              mr={2}
+              name={user.displayName}
+            />
+            <Box as='span' fontSize='sm' fontWeight='semibold'>
+              {user.displayName || user.email}
+            </Box>
+          </Flex>
+        ))}
+        <Button
+          variant='link'
+          size='xs'
+          variantColor='purple'
+          textTransform='uppercase'
+        >
+          + Add New
+        </Button>
+      </Box>
       <Flex alignItems='center'>
         <Button
           size='xs'
@@ -46,7 +75,7 @@ export default function ProjectTitle({
           &#9776;
         </Button>
         {!isEditing ? (
-          <Heading size='sm' onClick={() => setIsEditing(true)}>
+          <Heading size='md' onClick={() => setIsEditing(true)}>
             {projectName}
           </Heading>
         ) : (
