87fa72c5cb2c4667d38231d82fa76f0afbbead44
Ben Adam <benjamin.mark.adam@gmail.com>
2020-03-08 19:48:05 -0700

    Separate Add user per permissions

--- src/components/ProjectTitle.tsx
+++ src/components/ProjectTitle.tsx
@@ -1,10 +1,49 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { useForm } from 'react-hook-form';
 import { Heading, Flex, Input, Button, Avatar, Box } from '@chakra-ui/core';
 import ShareModal from './ShareModal';
 import AddUserModal from './AddUserModal';
+import { ProjectContext } from './ProjectContext';
 
 import { updateProject } from '../services/data';
+
+function ProjectTeammates({
+  users = [],
+  projectId,
+  projectName
+}: {
+  projectName: string;
+  projectId: string;
+  users?: Array<any>;
+}) {
+  const { permission } = useContext(ProjectContext);
+  return permission !== 'viewer' ? (
+    <Box position='absolute' top={24}>
+      <Heading as='h4' fontSize='12px' textTransform='uppercase' mb={4}>
+        Teammates:
+      </Heading>
+      {users.map((user: any) => (
+        <Flex key={user?.uid} mb={4} align='center'>
+          <Avatar
+            src={user.photoUrl}
+            display='block'
+            size='xs'
+            mr={2}
+            name={user.displayName}
+          />
+          <Box as='span' fontSize='sm' fontWeight='semibold'>
+            {user.displayName || user.email}
+          </Box>
+        </Flex>
+      ))}
+      <AddUserModal
+        users={users.map(user => user?.email)}
+        projectId={projectId}
+        projectName={projectName}
+      />
+    </Box>
+  ) : null;
+}
 
 export default function ProjectTitle({
   projectName,
@@ -38,30 +77,11 @@ export default function ProjectTitle({
       top='0px'
       p={8}
     >
-      <Box position='absolute' top={24}>
-        <Heading as='h4' fontSize='12px' textTransform='uppercase' mb={4}>
-          Teammates:
-        </Heading>
-        {users.map((user: any) => (
-          <Flex key={user?.uid} mb={4} align='center'>
-            <Avatar
-              src={user.photoUrl}
-              display='block'
-              size='xs'
-              mr={2}
-              name={user.displayName}
-            />
-            <Box as='span' fontSize='sm' fontWeight='semibold'>
-              {user.displayName || user.email}
-            </Box>
-          </Flex>
-        ))}
-        <AddUserModal
-          users={users.map(user => user?.email)}
-          projectId={projectId}
-          projectName={projectName}
-        />
-      </Box>
+      <ProjectTeammates
+        users={users}
+        projectName={projectName}
+        projectId={projectId}
+      />
       <Flex alignItems='center'>
         <Button
           size='xs'
