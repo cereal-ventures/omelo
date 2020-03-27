7621a5696674f3a9de70ffbfbaa52124a9794ae7
Ben Adam <benjamin.mark.adam@gmail.com>
2020-03-27 14:08:51 -0700

    Add welcome email

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,6 +7,23 @@ import * as functions from 'firebase-functions';
 //  response.send("Hello from Firebase!");
 // });
 
+exports.sendWelcomeEmail = functions.auth.user().onCreate(user => {
+  const sgMail = require('@sendgrid/mail');
+  sgMail.setApiKey(functions.config().sendgrid.key);
+  const msg = {
+    to: user?.email,
+    from: 'hello@omelo.com',
+    subject: 'Welcome to Omelo',
+    html: `Welcome to Omelo`,
+    templateId: 'd-8453e2f2d9854aebab1372a8e15f8088',
+    dynamic_template_data: {
+      name: user?.displayName,
+      url: 'https://beta.omelo.com'
+    }
+  };
+  sgMail.send(msg);
+});
+
 exports.sendProjectInvite = functions.firestore
   .document('invites/{inviteId}')
   .onCreate(async doc => {
@@ -20,13 +37,25 @@ exports.sendProjectInvite = functions.firestore
         : `${functions.config().url.base}?invite=${doc.id}&name=${encodeURI(
             invite?.projectName || ''
           )}`;
+
       const msg = {
         to: invite?.email,
         from: 'hello@omelo.com',
-        subject: 'You have been invited to join a project on Omelo.com',
-        text: `You have been invited to a project: ${url}`,
-        html: `You have been invited to a project: <a href="${url}" target="_blank">Accept invite</a>`
+        subject: `You have been invited to join ${invite?.projectName ||
+          'a project'} on Omelo.com`,
+        text: `You have been invited to ${invite?.projectName ||
+          'a project'} : ${url}`,
+        html: `You have been invited to ${invite?.projectName ||
+          'a project'} : <a href="${url}" target="_blank">Accept invite</a>`,
+        templateId: 'd-b697cb305e224eacac7e88a2c364950c',
+        dynamic_template_data: {
+          subject: `You have been invited to join ${invite?.projectName ||
+            'a project'} on Omelo.com`,
+          sender: invite?.sender,
+          url
+        }
       };
+
       sgMail.send(msg);
     }
   });
