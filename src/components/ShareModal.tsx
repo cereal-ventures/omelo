0dadec5ed923ac3db9856d32ca1831ecd0e4a2d2
Ben Adam <benjamin.mark.adam@gmail.com>
2020-04-03 15:45:02 -0700

    Update share modal styles

--- src/components/ShareModal.tsx
+++ src/components/ShareModal.tsx
@@ -18,10 +18,12 @@ import {
   Tooltip,
   Select,
   Divider,
-  ModalHeader
+  ModalHeader,
+  Icon,
+  Input,
+  Grid
 } from '@chakra-ui/core';
 import { updateProject, addUserToProject } from '../services/data';
-import FloatLabelInput from './FloatLabelInput';
 import { ProjectContext } from './ProjectContext';
 
 function ProjectTeammates({ users = [] }: { users?: Array<any> }) {
@@ -88,55 +90,43 @@ function InviteUserForm({
         onSubmit={handleSubmit(onSubmit)}
         autoComplete='off'
       >
-        <Flex align='center' flexDirection={{ xs: 'column', md: 'row' }}>
-          <Flex width='100%' position='relative'>
-            <FloatLabelInput
-              flexGrow={1}
-              width='100%'
-              name='email'
-              type='email'
-              label='Email Address'
-              error={errors?.email}
-              ref={register({
-                required: 'A valid email is required',
-                validate: value => {
-                  if (users.includes(value)) {
-                    return 'This user is already added to this project';
-                  }
-                }
-              })}
-            />
-            <Select
-              position='absolute'
-              top='8px'
-              right={0}
-              dir='rtl'
-              width='auto'
-              name='permission'
-              variant='unstyled'
-              ref={register}
-              zIndex={4}
-            >
-              <option value='viewer'>View Only</option>
-              <option value='commenter'>Can Comment</option>
-              {['owner', 'editor'].includes(permission) && (
-                <option value='editor'>Can Edit</option>
-              )}
-            </Select>
-          </Flex>
-          <Button
-            mt={{ xs: 4, md: 0 }}
-            px={8}
-            ml={4}
-            maxWidth='160px'
-            type='submit'
+        <Grid gridTemplateColumns={{ xs: '1fr', md: '2fr 1fr auto' }} gap={2}>
+          <Input
+            name='email'
+            type='email'
+            flexGrow={2}
+            width='100%'
+            placeholder='Email Address'
             size='sm'
-            variantColor='green'
-            borderRadius='full'
+            ref={register({
+              required: 'A valid email is required',
+              validate: value => {
+                if (users.includes(value)) {
+                  return 'This user is already added to this project';
+                }
+              }
+            })}
+          />
+          <Select
+            flexGrow={1}
+            width={{ xs: '100%', md: 'auto' }}
+            minWidth={{ md: '120px' }}
+            name='permission'
+            variant='outline'
+            size='sm'
+            ref={register}
+            zIndex={4}
           >
+            <option value='viewer'>View Only</option>
+            <option value='commenter'>Can Comment</option>
+            {['owner', 'editor'].includes(permission) && (
+              <option value='editor'>Can Edit</option>
+            )}
+          </Select>
+          <Button px={6} type='submit' size='sm' variantColor='purple'>
             Share
           </Button>
-        </Flex>
+        </Grid>
       </form>
     </Box>
   );
@@ -174,16 +164,14 @@ export default function ShareModal({
         <ProjectTeammates users={users} />
         <Button
           size='sm'
-          px={4}
-          color='brand.secondary'
           variant='outline'
-          borderRadius='full'
           onClick={() => {
             onOpen();
             updateProject({ projectId, payload: { isPublic: true } });
           }}
         >
-          Share Project
+          Share Timeline
+          <Icon name='link' size='.65em' ml={2} />
         </Button>
       </Flex>
       <Modal isOpen={isOpen} onClose={onClose} size='xl'>
@@ -198,7 +186,7 @@ export default function ShareModal({
                 color='brand.secondary'
                 fontWeight='semibold'
               >
-                Share this project
+                Share Timeline
               </Heading>
               <Text fontSize='md' fontWeight='light'>
                 Anyone with this link can view your timeline
@@ -207,12 +195,42 @@ export default function ShareModal({
           </ModalHeader>
           <Divider />
           <ModalBody py={4} px={8}>
-            <InviteUserForm
-              projectId={projectId}
-              projectName={projectName}
-              users={users}
-              onClose={onClose}
-            />
+            <Heading fontSize='12px' textTransform='uppercase' mb={4}>
+              Public Link
+            </Heading>
+            <Grid
+              alignItems='center'
+              gridTemplateColumns={{ xs: '1fr', md: '1fr auto' }}
+              gap={2}
+            >
+              <Link
+                fontWeight='semibold'
+                color='brand.secondary'
+                href={url}
+                target='blank'
+              >
+                {url}
+              </Link>
+              <Button
+                size='sm'
+                display={{ xs: 'none', md: 'block' }}
+                variantColor='purple'
+                fontWeight='semibold'
+                onClick={() => {
+                  setIsShared(true);
+                }}
+              >
+                {!isShared ? (
+                  <>
+                    Copy Link <Icon name='link' ml={2} size='.75em' />
+                  </>
+                ) : (
+                  <span role='img' aria-label='celebrate emoji'>
+                    Link Copied 🎉
+                  </span>
+                )}
+              </Button>
+            </Grid>
           </ModalBody>
           <Divider />
           <ModalBody py={4} px={8}>
@@ -235,44 +253,12 @@ export default function ShareModal({
           </ModalBody>
           <Divider />
           <ModalFooter px={8} pb={12} display='block'>
-            <Heading fontSize='12px' textTransform='uppercase'>
-              Public Link
-            </Heading>
-            <Flex
-              overflow='hidden'
-              flexDirection={{ xs: 'column', md: 'row' }}
-              align={{ xs: 'flex-start', md: 'center' }}
-              justify='space-between'
-              width='100%'
-              pt={4}
-            >
-              <Link
-                fontWeight='semibold'
-                color='brand.secondary'
-                href={url}
-                target='blank'
-              >
-                {url}
-              </Link>
-              <Button
-                px={0}
-                mt={{ xs: 4, md: 0 }}
-                variant='link'
-                variantColor='green'
-                fontWeight='semibold'
-                onClick={() => {
-                  setIsShared(true);
-                }}
-              >
-                {!isShared ? (
-                  <>Copy Link</>
-                ) : (
-                  <span role='img' aria-label='celebrate emoji'>
-                    🎉 Link Copied
-                  </span>
-                )}
-              </Button>
-            </Flex>
+            <InviteUserForm
+              projectId={projectId}
+              projectName={projectName}
+              users={users}
+              onClose={onClose}
+            />
           </ModalFooter>
         </ModalContent>
       </Modal>
